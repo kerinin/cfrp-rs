4b6a7fd592a20f789de1e299f9a069dc2c33fb1b
Ryan Michael <kerinin@gmail.com>
2015-04-12 09:53:41 -0500

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,21 @@
 use std::clone::*;
-
 use std::sync::mpsc::*;
+
+// ???
+pub trait Channel<A> {
+}
+
+pub trait Signal<A> {
+    fn lift<F, B>(&mut self, f: F) -> LiftedSignal<F, A, B> where F: Fn(&A) -> B;
+    fn foldp<F, B>(&mut self, f: F, b: B) -> FoldedSignal<F, A, B> where F: Fn(&A, B) -> B;
+}
 
 enum Memoized<A> {
     Changed(usize, A),
     Unchanged(usize),
 }
 
-pub trait Signal<A, B, F> 
-where F: Fn(A) -> B
-{
-    fn lift(&mut self, f: F) -> LiftedSignal<A, B, F>;
-}
-
-pub struct Channel<A> {
-    id: usize,
-    memo: Option<A>,
-    sinks: Vec<Sender<Memoized<A>>>,
-}
-
-pub struct LiftedSignal<A, B, F>
-where F: Fn(A) -> B
+pub struct LiftedSignal<F, A, B>
 {
     f: F,
     memo: Option<A>,
@@ -28,8 +23,46 @@ where F: Fn(A) -> B
     sinks: Vec<Sender<Memoized<B>>>,
 }
 
-impl<A, B, F> LiftedSignal<A, B, F> 
-where A: Clone + Eq, B: Clone, F: Fn(A) -> B
+pub struct FoldedSignal<F, A, B>
+{
+    f: F,
+    memo: Option<A>,
+    state: B,
+    source: Receiver<Memoized<A>>,
+    sinks: Vec<Sender<Memoized<B>>>,
+}
+
+impl<F, A, B> Signal<B> for LiftedSignal<F, A, B> 
+{
+    fn lift<G, C>(&mut self, g: G) -> LiftedSignal<G, B, C> where G: Fn(&B) -> C
+    {
+        let rx = self.subscribe();
+        LiftedSignal {f: g, memo: None, source: rx, sinks: Vec::new()}
+    }
+
+    fn foldp<G, C>(&mut self, g: G, c: C) -> FoldedSignal<G, B, C> where G: Fn(&B, C) -> C
+    {
+        let rx = self.subscribe();
+        FoldedSignal {f: g, memo: None, state: c, source: rx, sinks: Vec::new()}
+    }
+}
+
+impl<F, A, B> Signal<B> for FoldedSignal<F, A, B> 
+{
+    fn lift<G, C>(&mut self, g: G) -> LiftedSignal<G, B, C> where G: Fn(&B) -> C
+    {
+        let rx = self.subscribe();
+        LiftedSignal {f: g, memo: None, source: rx, sinks: Vec::new()}
+    }
+
+    fn foldp<G, C>(&mut self, g: G, c: C) -> FoldedSignal<G, B, C> where G: Fn(&B, C) -> C
+    {
+        let rx = self.subscribe();
+        FoldedSignal {f: g, memo: None, state: c, source: rx, sinks: Vec::new()}
+    }
+}
+
+impl<F, A, B> LiftedSignal<F, A, B> 
 {
     fn subscribe(&mut self) -> Receiver<Memoized<B>> {
         let (tx, rx) = channel();
@@ -42,10 +75,29 @@ where A: Clone + Eq, B: Clone, F: Fn(A) -> B
             sink.send(Memoized::Unchanged(id.clone()));
         }
     }
+}
 
-    fn push_changed(&mut self, id: usize, a: A) {
+impl<F, A, B> FoldedSignal<F, A, B> 
+{
+    fn subscribe(&mut self) -> Receiver<Memoized<B>> {
+        let (tx, rx) = channel();
+        self.sinks.push(tx);
+        return rx;
+    }
+
+    fn push_unchanged(&self, id: usize) {
+        for sink in self.sinks.iter() {
+            sink.send(Memoized::Unchanged(id.clone()));
+        }
+    }
+}
+
+impl<F, A, B> LiftedSignal<F, A, B> 
+where A: Clone + Eq, B: Clone, F: Fn(&A) -> B
+{
+    fn push_changed(&mut self, id: usize, a: &A) {
         self.memo = Some(a.clone());
-        let b = self.f.call_once((a.clone(),));
+        let b = (self.f)(a);
 
         for sink in self.sinks.iter() {
             sink.send(Memoized::Changed(id.clone(), b.clone()));
@@ -55,12 +107,45 @@ where A: Clone + Eq, B: Clone, F: Fn(A) -> B
     fn run(&mut self) {
         loop {
             let r = self.source.recv();
+            let memo = self.memo.clone(); // Workaround becasue need to borrow &self mutably
 
             match r {
                 Ok(msg) => {
-                    match msg {
-                        Memoized::Changed(id, a) => self.push_changed(id, a),
-                        Memoized::Unchanged(id) => self.push_unchanged(id),
+                    match (msg, memo) {
+                        (Memoized::Changed(id, ref a), None) => self.push_changed(id, a),
+                        (Memoized::Changed(id, ref a), Some(ref old_a)) if a != old_a => self.push_changed(id, a),
+                        (Memoized::Changed(id, _), _) | (Memoized::Unchanged(id), _) => self.push_unchanged(id),
+                    }
+                },
+                Err(_) => {},
+            }
+        }
+    }
+}
+
+impl<F, A, B> FoldedSignal<F, A, B> 
+where A: Clone + Eq, B: Clone, F: Fn(&A, B) -> B
+{
+    fn push_changed(&mut self, id: usize, a: &A) {
+        self.memo = Some(a.clone());
+        self.state = (self.f)(a, self.state.clone());
+
+        for sink in self.sinks.iter() {
+            sink.send(Memoized::Changed(id.clone(), self.state.clone()));
+        }
+    }
+
+    fn run(&mut self) {
+        loop {
+            let r = self.source.recv();
+            let memo = self.memo.clone(); // Workaround becasue need to borrow &self mutably
+
+            match r {
+                Ok(msg) => {
+                    match (msg, memo) {
+                        (Memoized::Changed(id, ref a), None) => self.push_changed(id, a),
+                        (Memoized::Changed(id, ref a), Some(ref old_a)) if a != old_a => self.push_changed(id, a),
+                        (Memoized::Changed(id, _), _) | (Memoized::Unchanged(id), _) => self.push_unchanged(id),
                     }
                 },
                 Err(_) => {},
@@ -118,7 +203,6 @@ where A: Clone + Eq, F: Fn(A) -> B
         return rx;
     }
 }
-*/
 
 #[test]
 fn it_works() {
@@ -133,3 +217,4 @@ fn it_works() {
     input_channel.send(1);
     input_channel.send(-1);
 }
+*/
