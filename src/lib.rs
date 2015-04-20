ff455c024ec18bc0db9562b64518d9eca0dbb2b9
Ryan Michael <kerinin@gmail.com>
2015-04-19 23:53:23 -0500

    Almost working with rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -1,271 +1,284 @@
-mod coordinator;
-mod lift;
-mod topology;
-
-pub use self::topology::Topology;
-pub use self::coordinator::Coordinator;
-pub use self::lift::Lift;
-
+use std::marker::*;
+use std::sync::*;
+use std::cell::*;
+use std::thread::spawn;
 use std::sync::mpsc::*;
 
-pub trait Spawn {
-    fn spawn(self: Box<Self>);
+#[derive(Debug, PartialEq, Eq)]
+pub enum Event<A> {
+    Changed(A),
+    NoChange,
+    Exit,
+}
+
+pub trait Signal<A>
+{
+    fn recv(&self) -> Event<A>;
+}
+
+pub trait Run {
+    fn run(self: Box<Self>);
 }
 
 trait NoOp: Send {
-    fn no_op(&self) -> bool;
+    fn send_no_change(&self);
 }
 
-pub trait Signal<A> {
-    fn publish_to(&self, Sender<Option<A>>);
+trait CoordinatedInput: Send {
+    fn run(self: Box<Self>, usize, Arc<Mutex<Vec<Box<NoOp>>>>);
+    fn boxed_no_op(&self) -> Box<NoOp>;
 }
 
-impl<A> NoOp for Sender<Option<A>> 
-where A: 'static + Send
+struct Input<A> {
+    source_rx: Receiver<A>,
+    sink_tx: Sender<Event<A>>,
+}
+
+impl<A> CoordinatedInput for Input<A> where
+    A: 'static + Clone + Send,
 {
-    fn no_op(&self) -> bool {
-        match self.send(None) {
-            Err(_) => true,
-            _ => false,
+    fn run(self: Box<Self>, idx: usize, no_ops: Arc<Mutex<Vec<Box<NoOp>>>>) {
+        loop {
+            match self.source_rx.recv() {
+                Ok(ref a) => {
+                    for (i, ref no_op) in no_ops.lock().unwrap().iter().enumerate() {
+                        if i == idx {
+                            self.sink_tx.send(Event::Changed(a.clone()));
+                        } else {
+                            no_op.send_no_change();
+                        }
+                    }
+                },
+                Err(_) => {
+                    for (i, no_op) in no_ops.lock().unwrap().iter().enumerate() {
+                        if i == idx {
+                            self.sink_tx.send(Event::Exit);
+                        } else {
+                            no_op.send_no_change();
+                        }
+                    }
+                    return;
+                },
+            }
+        }
+    }
+
+    fn boxed_no_op(&self) -> Box<NoOp> {
+        Box::new(self.sink_tx.clone())
+    }
+}
+
+impl<A> NoOp for Sender<Event<A>> where
+    A: 'static + Send,
+{
+    fn send_no_change(&self) {
+        self.send(Event::NoChange);
+    }
+}
+
+pub struct Channel<A> {
+    source_rx: Receiver<Event<A>>,
+}
+
+impl<A> Signal<A> for Channel<A>
+{
+    fn recv(&self) -> Event<A> {
+        match self.source_rx.recv() {
+            Err(_) => Event::Exit,
+            Ok(a) => a,
+        }
+    }
+}
+
+impl<A> Channel<A>
+{
+    pub fn lift<F, B>(self, f: F) -> Lift<F, A, B> where
+        F: Fn(&A) -> B,
+        A: 'static,
+        B: 'static,
+    {
+        Lift {
+            parent: Box::new(self),
+            f: f,
+        }
+    }
+}
+
+pub struct Lift<F, A, B> where F: Fn(&A) -> B {
+    parent: Box<Signal<A>>,
+    f: F,
+}
+
+impl<F, A, B> Signal<B> for Lift<F, A, B> where
+    F: Fn(&A) -> B,
+{
+    fn recv(&self) -> Event<B> {
+       match self.parent.recv() {
+           Event::Changed(ref a) => Event::Changed((self.f)(a)),
+           Event::NoChange => Event::NoChange,
+           Event::Exit => Event::Exit,
+       }
+    }
+}
+
+impl<F, A, B> Run for Lift<F, A, B> where
+    F: Fn(&A) -> B,
+{
+    fn run(self: Box<Self>) {
+        loop {
+            self.recv();
+        }
+    }
+}
+
+pub struct Fork<A> {
+    parent: Box<Signal<A>>,
+    sink_txs: Arc<Mutex<Vec<Sender<Event<A>>>>>,
+}
+
+impl<A> Run for Fork<A> where
+    A: Clone,
+{
+    fn run(self: Box<Self>) {
+        loop {
+            match self.parent.recv() {
+                Event::Changed(a) => {
+                    for sink in self.sink_txs.lock().unwrap().iter() {
+                        sink.send(Event::Changed(a.clone()));
+                    }
+                },
+                _ => {},
+            }
+        }
+    }
+}
+
+pub struct Branch<A> where
+    A: 'static
+{
+    fork_txs: Arc<Mutex<Vec<Sender<Event<A>>>>>,
+    source_rx: Receiver<Event<A>>,
+}
+
+impl<A> Clone for Branch<A> {
+    fn clone(&self) -> Branch<A> {
+        let (tx, rx) = channel();
+        self.fork_txs.lock().unwrap().push(tx);
+        Branch { fork_txs: self.fork_txs.clone(), source_rx: rx }
+    }
+}
+
+impl<A> Signal<A> for Branch<A>
+{
+    fn recv(&self) -> Event<A> {
+        match self.source_rx.recv() {
+            Err(_) => Event::Exit,
+            Ok(a) => a,
         }
     }
 }
 
 
-pub fn lift<F, A, B>(f: F, a: &Signal<A>) -> Lift<F, A, B> where
-    F: Fn(&A) -> B,
-{
-    let (tx, rx) = channel();
-    a.publish_to(tx);
-    Lift::new(Box::new(f), rx)
+pub struct Builder {
+    inputs: RefCell<Vec<Box<CoordinatedInput>>>,
+    root_signals: RefCell<Vec<Box<Run>>>,
 }
-    
-/*
-use std::thread;
-use std::sync::mpsc::*;
 
-fn lift<'a, F, A, B>(f: F, signal: &Signal<'a, A>) -> Signal<'a, B> where 
-    F: 'static + Send + Clone + Fn(&A) -> B,
-    A: 'static + Send + Clone,
-    B: 'static + Send + Clone + Eq,
-{
-    // NOTE: Check for coordinator match
-    let (in_tx, in_rx) = channel();
-    signal.publish_to(in_tx);
+impl Builder {
+    pub fn add<A>(&self, root: Box<Signal<A>>) -> Branch<A> where
+        A: Clone,
+    {
+        let (tx, rx) = channel();
+        let fork_txs = Arc::new(Mutex::new(vec![tx]));
 
-    let (signal_tx, signal_rx) = channel();
-    thread::spawn(move || {
-        let mut last_b = None;
-        let mut send_if_changed = |a: &A| -> bool {
-            let b = Some(f(a));
-
-            let value = if last_b == b {
-                None
-            } else {
-                last_b = b.clone();
-                b
-            };
-
-            match signal_tx.send(value) {
-                Err(_) => { true }
-                _ => { false }
-            }
+        let fork = Fork {
+            parent: root,
+            sink_txs: fork_txs.clone(),
         };
 
-        loop {
-            match in_rx.recv() {
-                // Signal value changed
-                Ok(Some(ref a)) => {
-                    if send_if_changed(a) { break }
-                }
-                
-                // No change from previous - send it along!
-                Ok(None) => {
-                    match signal_tx.send(None) {
-                        Err(_) => { break }
-                        _ => {}
-                    }
-                }
+        self.root_signals.borrow_mut().push(Box::new(fork));
 
-                // Receiver closed, time to pack up & go home
-                _ => { break; }
-            }
-        }
-    });
+        Branch { fork_txs: fork_txs, source_rx: rx }
+    }
 
-    Signal::new(&signal.coordinator, signal_rx)
-}
-
-fn lift2<'a, F, A, B, C>(f: F, left: &Signal<'a, A>, right: &Signal<'a, B>) -> Signal<'a, C> where 
-    F: 'static + Send + Clone + Fn(&A, &B) -> C,
-    A: 'static + Send + Clone,
-    B: 'static + Send + Clone,
-    C: 'static + Send + Clone + Eq,
-{
-    // NOTE: Check for coordinator match
-    let (left_tx, left_rx) = channel();
-    left.publish_to(left_tx);
-
-    let (right_tx, right_rx) = channel();
-    right.publish_to(right_tx);
-
-    let (signal_tx, signal_rx) = channel();
-    thread::spawn(move || {
-        let mut last_a = None;
-        let mut last_b = None;
-        let mut last_c = None;
-        let mut send_if_changed = |a: &A, b: &B| -> bool {
-            let c = Some(f(a, b));
-
-            let value = if last_c == c {
-                None
-            } else {
-                last_c = c.clone();
-                c
-            };
-
-            match signal_tx.send(value) {
-                Err(_) => { true }
-                _ => { false }
-            }
+    pub fn channel<A>(&self, source_rx: Receiver<A>) -> Channel<A> where
+        A: 'static + Clone + Send,
+    {
+        let (tx, rx) = channel();
+        let input = Input {
+            source_rx: source_rx,
+            sink_tx: tx,
         };
 
-        loop {
-            match (left_rx.recv(), right_rx.recv()) {
-                (Ok(Some(ref a)), Ok(Some(ref b))) => {
-                    if send_if_changed(a, b) { break };
-                }
+        self.inputs.borrow_mut().push(Box::new(input));
 
-                (Ok(None), Ok(Some(ref b))) => {
-                    let a = match last_a.clone() {
-                        Some(a) => { a }
-                        None => { panic!("Channel reports no change, but nothing was cached") }
-                    };
-                    if send_if_changed(a, b) { break };
-                }
-
-                (Ok(Some(ref a)), Ok(None)) => {
-                    let b = match last_b.clone() {
-                        Some(b) => { b }
-                        None => { panic!("Channel reports no change, but nothing was cached") }
-                    };
-                    if send_if_changed(a, b) { break };
-                }
-
-                (Ok(None), Ok(None)) => {
-                    match signal_tx.send(None) {
-                        Err(_) => { break }
-                        _ => {}
-                    }
-                }
-
-                _ => { break; }
-            }
+        Channel {
+            source_rx: rx,
         }
-    });
-
-    Signal::new(&left.coordinator, signal_rx)
+    }
 }
 
-fn foldp<'a, F, A, B>(f: F, initial: B, signal: &Signal<'a, A>) -> Signal<'a, B> where 
-    F: 'static + Send + Clone + Fn(&B, &A) -> B,
-    A: 'static + Send + Clone,
-    B: 'static + Send + Clone + Eq,
-{
-    let (in_tx, in_rx) = channel();
-    signal.publish_to(in_tx);
-
-    let (signal_tx, signal_rx) = channel();
-    thread::spawn(move || {
-        let mut state = initial;
-        let mut last_b = None;
-
-        let mut send_if_changed = |a: &A| -> bool {
-            let b = Some(f(&state, a));
-
-            let value = if last_b == b {
-                None
-            } else {
-                last_b = b.clone();
-                b
-            };
-
-            match signal_tx.send(value) {
-                Err(_) => { true }
-                _ => { false }
-            }
-        };
-
-        loop {
-            match in_rx.recv() {
-                Ok(Some(ref a)) => {
-                    if send_if_changed(a) { break }
-                }
-
-                Ok(None) => {
-                    match signal_tx.send(None) {
-                        Err(_) => { break }
-                        _ => {}
-                    }
-                }
-
-                _ => { break; }
-            }
-        }
-    });
-
-    Signal::new(&signal.coordinator, signal_rx)
+pub struct Topology {
+    builder: Builder,
 }
 
-fn async<'a, A>(signal: &Signal<'a, A>) -> Signal<'a, A> where
-    A: 'static + Send + Clone,
-{
-    let (in_tx, in_rx) = channel();
-    signal.publish_to(in_tx);
+impl Topology {
+    pub fn build<F>(mut f: F) -> Self where 
+        F: Fn(&Builder),
+    {
+        let builder = Builder { root_signals: RefCell::new(Vec::new()), inputs: RefCell::new(Vec::new()) };
+        f(&builder);
+        
+        Topology { builder: builder }
+    }
 
-    let (channel_tx, signal) = signal.coordinator.channel();
-    thread::spawn(move || {
-        loop {
-            match in_rx.recv() {
-                Ok(Some(ref a)) => {
-                    channel_tx.send(a.clone());
-                }
+    pub fn run(self) {
+        let Builder {inputs, root_signals} = self.builder;
 
-                Ok(None) => {}
-
-                _ => { break }
-            }
+        /*
+        for root_signal in root_signals.into_iter() {
+            spawn(move || {
+                root_signal.run();
+            });
         }
-    });
 
-    signal
+        */
+        let no_ops = Arc::new(Mutex::new(inputs.borrow().iter().map(|i| i.boxed_no_op()).collect::<Vec<Box<NoOp>>>()));
+        for (idx, input) in inputs.into_inner().into_iter().enumerate() {
+            let no_ops_i = no_ops.clone();
+            spawn(move || {
+                input.run(idx, no_ops_i);
+            });
+        }
+    }
 }
 
-*/
 
 #[cfg(test)] 
 mod test {
     // extern crate quickcheck;
-    
     use std::sync::mpsc::*;
-
+    
     use super::*;
 
     #[test]
     fn integration() {
-        let coordinator = Coordinator::new();
-        let (in_tx, in_rx): (Sender<usize>, Receiver<usize>) = channel();
-        let s_1 = coordinator.channel(in_rx);
-        let s_2 = lift(|i: &usize| -> usize { i + 1 }, &s_1);
-        let (out_tx, out_rx): (Sender<Option<usize>>, Receiver<Option<usize>>) = channel();
-        s_2.publish_to(out_tx);
+        let (out_tx, out_rx): (Sender<Event<usize>>, Receiver<Event<usize>>) = channel();
 
-        let mut topology = Topology::new(coordinator);
-        topology.add_node(Box::new(s_1));
-        topology.add_node(Box::new(s_2));
-        topology.run();
+        Topology::build(|t: &Builder| {
+            let (in_tx, in_rx): (Sender<usize>, Receiver<usize>) = channel();
 
-        in_tx.send(0);
-        assert_eq!(Some(1), out_rx.recv().unwrap())
+            let plus_one = t.add(Box::new(
+                t.channel(in_rx)
+                    .lift(|i: &usize| -> usize { i + 1 })
+            ));
+            // t.add(Box::new(
+            //     plus_one.
+            //         lift(|i: &usize| -> usize { i + 1 })
+            // ));
+        }).run();
+
+        // in_tx.send(0);
+        // assert_eq!(Event::Changed(1), out_rx.recv().unwrap())
     }
 }
