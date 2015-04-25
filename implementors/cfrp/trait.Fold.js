2dececa98290f5ecc68d8e150cdc2e2e5a94c4f8
Ryan Michael <kerinin@gmail.com>
2015-04-25 15:28:46 +0000

    rebuild docs at a9ca380

---
+++ implementors/cfrp/trait.Fold.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['cfrp'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
