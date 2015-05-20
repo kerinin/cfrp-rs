63f6fd2accf208a5cfd7c35c58552d83439219bb
Ryan Michael <kerinin@gmail.com>
2015-05-20 19:12:33 +0000

    rebuild docs at 0dc1b6d

--- implementors/core/iter/trait.DoubleEndedIterator.js
+++ implementors/core/iter/trait.DoubleEndedIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['cfrp'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='enum' href='cfrp/enum.Value.html' title='cfrp::Value'>Value</a>&lt;T&gt;",];
+implementors['log'] = ["impl&lt;I&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='http://doc.rust-lang.org/nightly/alloc/boxed/struct.Box.html' title='alloc::boxed::Box'>Box</a>&lt;I&gt;",];implementors['cfrp'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='enum' href='cfrp/enum.Value.html' title='cfrp::Value'>Value</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
