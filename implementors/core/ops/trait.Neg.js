e5eb3c6fb98be70aaee637a7519f937a1f88ca9e
Ryan Michael <kerinin@gmail.com>
2015-08-22 15:50:09 +0000

    rebuild docs at 6a2e818

--- implementors/core/ops/trait.Neg.js
+++ implementors/core/ops/trait.Neg.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['time'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];
+implementors['time'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
